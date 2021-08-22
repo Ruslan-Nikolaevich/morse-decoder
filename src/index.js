e3f28ba776d5eea89708e3fd229742b44b2bd9dc
Ruslan-Nikolaevich <ruslan_nic@mail.ru>
2021-08-22 23:55:09 +0300

    Morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,66 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+// -----------------------------
+let mas=[];
+let strrez="";
+for (let i = 0; i < expr.length; i+=10) {   // разбиваем сроку на массив слов по 10 символов
+  mas.push(expr.substring(i,i+10));
+  
+}
+//--------------------------------------------------------функция для  конвертации dec в морзянку + пробелы 
+function mor(str) {
+let twochar="";
+let morze="";
+let rez;
+    for (let i = 0; i < str.length ; i+=2) {
+        twochar=str.substring(i,i+2);
+        if (twochar==="**") {
+            return morze=" ";
+        } else{
+            twochar==="10" ?  morze+="." : false;
+            twochar==="11" ?  morze+="-" : false;
+        }
+        
+    }
+    //  console.log ("ф-я mor"+morze);
+     return morze;
+      
+}
+//----------------------------------------------------------- конвертируем морзянку в букву
+function decstr(str) {
+  let slovo="";
+  let s=str;
+//   console.log("переданна строка "+s);
+    for (let key in MORSE_TABLE) {
+        // console.log(key);
+        if (key===str) {
+            // console.log("вход в цикл");
+            slovo = MORSE_TABLE[key];
+            //  console.log("присвоено значение "+slovo);
+        }
+        
+    }
+    // console.log(slovo);
+    return slovo;
+}
+
+
+//-----------------------------------------------------------  собираем все стоку морзянки и пробелы 
+for (let i = 0; i < mas.length ; i++) {
+    if (mor(mas[i])===" ") {
+        strrez+=mor(mas[i]);
+        // console.log ("добавили пробел");
+    } else {
+        let temp = mor(mas[i]);
+        strrez+= decstr(temp);
+    }
+   
+    
+}
+// console.log(strrez);
+return strrez;
+//----------------------
 }
 
 module.exports = {
